a244e722d146fb2d91facf3d80cefa47aedfad6a
gunngreg <gunngregory43@gmail.com>
2024-08-08 01:16:39 +0100

    Update simple-contract.sh

--- simple-contract.sh
+++ simple-contract.sh
@@ -1,62 +1,61 @@
-#!/bin/sh
+#!/bin/bash
 
-wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
-curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
-sleep 4
+print_blue() {
+    echo -e "\033[34m$1\033[0m"
+}
 
-sudo apt-get update && sudo apt get upgrade -y
-clear
+prompt_for_input() {
+    read -p "$1" input
+    echo $input
+}
 
-echo "Installing Hardhat and dotenv..."
-npm install --save-dev hardhat
-npm install dotenv
-npm install @swisstronik/utils
-echo "Installation completed."
+print_blue "Installing Hardhat and necessary dependencies..."
+echo
+npm install --save-dev hardhat @nomicfoundation/hardhat-toolbox
+echo
 
-echo "Creating a Hardhat project..."
+print_blue "Initializing Hardhat project..."
+echo
 npx hardhat
+echo
 
-rm -f contracts/Lock.sol
-echo "Lock.sol removed."
+print_blue "Removing the default Hardhat configuration file..."
+echo
+rm hardhat.config.js
+echo
+read -p "Enter your wallet private key: " PRIVATE_KEY
 
-echo "Hardhat project created."
+if [[ $PRIVATE_KEY != 0x* ]]; then
+  PRIVATE_KEY="0x$PRIVATE_KEY"
+fi
 
-echo "Installing Hardhat toolbox..."
-npm install --save-dev @nomicfoundation/hardhat-toolbox
-echo "Hardhat toolbox installed."
-
-echo "Creating .env file..."
-read -p "Enter your private key: " PRIVATE_KEY
-echo "PRIVATE_KEY=$PRIVATE_KEY" > .env
-echo ".env file created."
-
-echo "Configuring Hardhat..."
 cat <<EOL > hardhat.config.js
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.19",
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [\`0x\${process.env.PRIVATE_KEY}\`],
+      accounts: ["$PRIVATE_KEY"],
     },
   },
 };
 EOL
-echo "Hardhat configuration completed."
 
-echo "Creating Hello_swtr.sol contract..."
-mkdir -p contracts
-cat <<EOL > contracts/Hello_swtr.sol
+print_blue "Hardhat configuration file has been updated."
+echo
+
+rm -f contracts/Lock.sol
+sleep 2
+cat <<EOL > contracts/Lock.sol
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
 contract Swisstronik {
     string private message;
 
-    constructor(string memory _message) payable {
+    constructor(string memory _message) payable{
         message = _message;
     }
 
@@ -64,111 +63,51 @@ contract Swisstronik {
         message = _message;
     }
 
-    function getMessage() public view returns(string memory) {
+    function getMessage() public view returns(string memory){
         return message;
     }
 }
 EOL
-echo "Hello_swtr.sol contract created."
+echo
 
-echo "Compiling the contract..."
+print_blue "Compiling the contract..."
+echo
 npx hardhat compile
-echo "Contract compiled."
+echo
 
-echo "Creating deploy.js script..."
+print_blue "Creating scripts directory and the deployment script..."
+echo
+
 mkdir -p scripts
+
 cat <<EOL > scripts/deploy.js
 const hre = require("hardhat");
 
 async function main() {
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik"]);
+
+  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
+
   await contract.waitForDeployment();
-  console.log(\`Swisstronik contract deployed to \${contract.target}\`);
-}
 
+  console.log(\`This is your deployed contract address \${contract.target}\`);
+}
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
 EOL
-echo "deploy.js script created."
+echo
 
-echo "Deploying the contract..."
 npx hardhat run scripts/deploy.js --network swisstronik
-echo "Contract deployed."
+echo
 
-echo "Creating setMessage.js script..."
-cat <<EOL > scripts/setMessage.js
-const hre = require("hardhat");
-const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
-
-const sendShieldedTransaction = async (signer, destination, data, value) => {
-  const rpclink = hre.network.config.url;
-  const [encryptedData] = await encryptDataField(rpclink, data);
-  return await signer.sendTransaction({
-    from: signer.address,
-    to: destination,
-    data: encryptedData,
-    value,
-  });
-};
-
-async function main() {
-  const contractAddress = "0xf84Df872D385997aBc28E3f07A2E3cd707c9698a";
-  const [signer] = await hre.ethers.getSigners();
-  const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
-  const contract = contractFactory.attach(contractAddress);
-  const functionName = "setMessage";
-  const messageToSet = "Hello Swisstronik";
-  const setMessageTx = await sendShieldedTransaction(signer, contractAddress, contract.interface.encodeFunctionData(functionName, [messageToSet]), 0);
-  await setMessageTx.wait();
-  console.log("Transaction Receipt: ", setMessageTx);
-}
-
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
-EOL
-echo "setMessage.js script created."
-
-echo "Running setMessage.js..."
-npx hardhat run scripts/setMessage.js --network swisstronik
-echo "Message set."
-
-echo "Creating getMessage.js script..."
-cat <<EOL > scripts/getMessage.js
-const hre = require("hardhat");
-const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
-
-const sendShieldedQuery = async (provider, destination, data) => {
-  const rpclink = hre.network.config.url;
-  const [encryptedData, usedEncryptedKey] = await encryptDataField(rpclink, data);
-  const response = await provider.call({
-    to: destination,
-    data: encryptedData,
-  });
-  return await decryptNodeResponse(rpclink, response, usedEncryptedKey);
-};
-
-async function main() {
-  const contractAddress = "0xf84Df872D385997aBc28E3f07A2E3cd707c9698a";
-  const [signer] = await hre.ethers.getSigners();
-  const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
-  const contract = contractFactory.attach(contractAddress);
-  const functionName = "getMessage";
-  const responseMessage = await sendShieldedQuery(signer.provider, contractAddress, contract.interface.encodeFunctionData(functionName));
-  console.log("Decoded response:", contract.interface.decodeFunctionResult(functionName, responseMessage)[0]);
-}
-
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
-EOL
-echo "getMessage.js script created."
-
-echo "Running getMessage.js..."
-npx hardhat run scripts/getMessage.js --network swisstronik
-echo "Message retrieved."
-echo "Done!"
+print_blue "Simple Contract deployment successful, Copy the above contract address and save it somewhere, you need to submit it in Testnet website"
+sed -i 's/0x[0-9a-fA-F]*,\?\s*//g' hardhat.config.js
+echo
+print_blue "PRIVATE_KEY has been removed from hardhat.config.js."
+echo
+print_blue "Pushing these files to your github Repo link"
+git add . && git commit -m "Initial commit" && git push origin main
+echo
+print_blue "-----------------------"
+echo
